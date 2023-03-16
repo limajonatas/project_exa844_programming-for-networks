e173da5632a594e4afcd62b1160753a4e4af579c
Jonatas <jonataslimafsa@gmail.com>
2023-03-15 23:20:11 -0300

    uncomment

--- project - part 1.py
+++ project - part 1.py
@@ -7,5 +7,6 @@ import dataByGen
 urlPais = "https://sig.tse.jus.br/ords/dwapr/seai/r/sig-eleicao-resultados/maiores-votacoes?p0_turno=1&session=214694431999301"
 urlRegiao = "https://sig.tse.jus.br/ords/dwapr/seai/r/sig-eleicao-resultados/maiores-votacoes?p0_abrangencia=Regi%C3%A3o&clear=RP&session=205449690850665"
 urlByGenero = 'https://sig.tse.jus.br/ords/dwapr/seai/r/sig-eleicao-resultados/g%C3%AAnero?session=208821330692616'
-#dataCountry.getData(urlPais)
+
+dataCountry.getData(urlPais)
 dataByGen.getData(urlByGenero)
