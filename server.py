50e06c1c05561afcc25301299498ea5ddaf6735c
Jonatas <44263115+limajonatas@users.noreply.github.com>
2023-04-12 11:52:28 -0300

    :sparkles: get data by education level in server

--- server.py
+++ server.py
@@ -4,41 +4,78 @@ from flask import Flask
 app = Flask(__name__)
 
 # Lê o arquivo JSON
-with open('dataAll.json', 'r') as f:
+with open("dataAll.json", "r") as f:
     data = json.load(f)
 
+
 # Define uma rota que retorna o conteúdo do arquivo JSON
-@app.route('/')
+@app.route("/")
 def get_data():
-    return data
+    if data:
+        return data
+    else:
+        return "Não encontrado"
 
-# Define uma rota que retorna o conteúdo do arquivo JSON por Raça
-@app.route('/race')
+
+# Define uma rota que retorna o conteúdo do arquivo JSON por Cor/Raça
+@app.route("/race")
 def get_race():
-    return data['by_race']
+    by_race = data["by_race"]
+    if by_race:
+        return by_race
+    else:
+        return "Não encontrado"
+
 
 # Define uma rota que retorna o conteúdo do arquivo JSON por gênero
-@app.route('/gender')
+@app.route("/gender")
 def get_gender():
-    return data['by_gender']
+    by_gender = data["by_gender"]
+    if by_gender:
+        return by_gender
+    else:
+        return "Não encontrado"
 
-# Define uma rota que retorna o conteúdo do arquivo JSON por gênero
-@app.route('/gender/<year>')
+
+# Define uma rota que retorna o conteúdo do arquivo JSON por grau de instrução
+@app.route("/educationLevel")
+def get_education_level():
+    by_education_level = data["by_education_level"]
+    if by_education_level:
+        return by_education_level
+    else:
+        return "Não encontrado"
+
+
+# Define uma rota que retorna o conteúdo do arquivo JSON por gênero e ano
+@app.route("/gender/<year>")
 def get_gender_by_year(year):
-    value = [aux for aux in data['by_gender'] if aux['ano'] == year]
-    if(value):
+    value = [aux for aux in data["by_gender"] if aux["ano"] == year]
+    if value:
         return value[0]
     else:
-        return 'Não encontrado'
+        return "Não encontrado"
 
-# Define uma rota que retorna o conteúdo do arquivo JSON por gênero
-@app.route('/race/<year>')
+
+# Define uma rota que retorna o conteúdo do arquivo JSON por cor/raça e ano
+@app.route("/race/<year>")
 def get_race_by_year(year):
-    value = [aux for aux in data['by_race'] if aux['ano'] == year]
-    if(value):
+    value = [aux for aux in data["by_race"] if aux["ano"] == year]
+    if value:
         return value[0]
     else:
-        return 'Não encontrado'
+        return "Não encontrado"
 
-if __name__ == '__main__':
+
+# Define uma rota que retorna o conteúdo do arquivo JSON por grau de instrução e ano
+@app.route("/educationLevel/<year>")
+def get_education_level_by_year(year):
+    value = [aux for aux in data["by_education_level"] if aux["ano"] == year]
+    if value:
+        return value[0]
+    else:
+        return "Não encontrado"
+
+
+if __name__ == "__main__":
     app.run()
