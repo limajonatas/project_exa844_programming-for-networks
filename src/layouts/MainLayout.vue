297524dfda7a18386897d1b6f002ca0de2c3874b
Jonatas <44263115+limajonatas@users.noreply.github.com>
2023-05-10 23:50:29 -0300

    :construction: toolbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -9,31 +9,23 @@
           icon="menu"
           aria-label="Menu"
           @click="toggleLeftDrawer"
+          v-if="$route.meta.subPage"
         />
         <q-toolbar-title class="text-right">
           {{ $route.meta.title }}
         </q-toolbar-title>
-        <q-toolbar-title class="text-right">
+        <q-toolbar-title class="text-right text-bold text-black text-h5">
           Estatísticas Eleições Brasileira
         </q-toolbar-title>
-
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" v-if="$route.meta.subPage" bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+        <q-item-label header> Menu </q-item-label>
 
         <EssentialLink
-          v-for="link in essentialLinks"
+          v-for="link in linksList"
           :key="link.title"
           v-bind="link"
         />
@@ -47,71 +39,62 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-// import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+import { defineComponent, ref, computed } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+import { useRoute } from "vue-router";
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
-    EssentialLink
+    EssentialLink,
   },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const route = useRoute();
+    const linksList = computed(() => {
+      let data = [
+        {
+          title: "Dados Gerais",
+          icon: "home",
+          link: "",
+        },
+        {
+          title: "Por Gênero",
+          icon: "people",
+          link: "",
+        },
+        {
+          title: "Por Grau de Instrução",
+          icon: "school",
+          link: "",
+        },
+        {
+          title: "Por Cor/Raça",
+          icon: "groups",
+          link: "",
+        },
+      ];
 
+      if (route.name == "all-data")
+        data = data.filter((item) => item.title != "Dados Gerais");
+      else if (route.name == "per-gender")
+        data = data.filter((item) => item.title != "Por Gênero");
+      else if (route.name == "per-education")
+        data = data.filter((item) => item.title != "Por Grau de Instrução");
+      else if (route.name == "per-race")
+        data = data.filter((item) => item.title != "Por Cor/Raça");
+
+      return data;
+    });
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+      linksList,
+    };
+  },
+});
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,16 +3,23 @@ const routes = [
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: "", component: () => import("pages/IndexPage.vue") },
+      {
+        path: "",
+        component: () => import("pages/IndexPage.vue"),
+        meta: {
+          title: "Home",
+          subPage: false,
+        },
+      },
       {
         path: "/dados-gerais",
         name: "all-data",
         component: () => import("src/pages/statistics/AllStatistics.vue"),
         meta: {
           title: "Dados Gerais",
-          subPage: true
-        }
-      }
+          subPage: true,
+        },
+      },
     ],
   },
 
