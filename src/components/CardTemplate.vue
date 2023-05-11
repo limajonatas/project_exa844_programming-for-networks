363e526920ec9a1e06f8948b290a48107114fc20
Jonatas <44263115+limajonatas@users.noreply.github.com>
2023-05-10 22:37:05 -0300

    :sparkles: home

--- src/components/CardTemplate.vue
+++ src/components/CardTemplate.vue
@@ -1,42 +1,48 @@
 <template>
-  <q-card>
-    <q-card-section :class="size == 'large' ? 'q-px-xl' : 'q-pa-sm'">
-      <div
-        v-if="size == 'large'"
-        class="text-bold q-py-none"
-        style="font-size: 50pt"
-      >
-        {{ text }}
-      </div>
-      <div
-        v-else-if="size == 'small'"
-        class="text-bold q-py-none"
-        style="font-size: 20pt"
-      >
-        {{ text }}
-      </div>
-      <div v-else class="text-bold q-py-none" style="font-size: 30pt">
-        {{ text }}
-      </div>
-    </q-card-section>
+  <q-card style="border: solid grey 1px" class="card">
+    <div
+      class="text-bold q-pl-md text-white text"
+      :class="[
+        { 'card-large': size == 'large' },
+        { 'card-small': size == 'small' },
+        { 'card-medium': size == 'medium' },
+      ]"
+      style="position: absolute; top: 0; left: 0; z-index: 1"
+    >
+      {{ text }}
+    </div>
+    <q-img
+      :src="image"
+      :class="[
+        { 'image-large': size == 'large' },
+        { 'image-small': size == 'small' },
+        { 'image-medium': size == 'medium' },
+      ]"
+      fit="fill"
+    />
   </q-card>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+
 export default defineComponent({
   name: "CardTemplate",
   props: {
     size: {
       type: String,
-      required: false,
+      default: "medium",
     },
     text: {
       type: String,
-      required: false,
+      default: "Title Here",
+    },
+    image: {
+      type: String,
     },
   },
   setup() {
+    const random = Math.floor(Math.random() * 360); // Gera um valor aleatório entre 0 e 359
     return {};
   },
   components: {},
@@ -45,7 +51,39 @@ export default defineComponent({
 
 <style lang="css" scoped>
 .q-card:hover {
-  transform: scale(1.1); /* Aumenta 5% o tamanho do card */
+  transform: scale(1.08); /* Aumenta 5% o tamanho do card */
   transition: transform 0.3s ease; /* Adiciona um efeito de transição suave */
+}
+.q-card {
+  position: relative;
+}
+.text {
+  text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black,
+    1px 1px 0 black;
+}
+.card-small {
+  padding-top: 8px;
+  font-size: 20pt;
+}
+.card-medium {
+  padding-top: 8px;
+  font-size: 35pt;
+}
+
+.card-large {
+  font-size: 50pt;
+}
+.image-small {
+  height: 138px;
+  filter: blur(1.5px);
+  filter: blur(1.5px) hue-rotate(90deg);
+}
+.image-medium {
+  height: 240px;
+  filter: blur(1.5px);
+}
+.image-large {
+  height: 602px;
+  filter: blur(1.5px) hue-rotate(180deg);
 }
 </style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,27 +1,14 @@
 <template>
   <q-page class="home">
     <card-template
-      :size="$q.platform.is.mobile ? 'md' : 'large'"
-      class="home__card-large"
-      text="Todos os Dados"
-    >
-    </card-template>
-    <card-template
-      :size="$q.platform.is.mobile ? 'small' : ''"
-      class="home_card-second"
-      text="Por Gênero"
-    >
-    </card-template>
-    <card-template
-      :size="$q.platform.is.mobile ? 'small' : ''"
-      class="home_card-third"
-      text="Por Cor/Raça"
-    >
-    </card-template>
-    <card-template
-      class="home_card-fourth"
-      text="Por Escolaridade"
-      :size="$q.platform.is.mobile ? 'small' : ''"
+      style="height: 100%"
+      v-for="card in cards"
+      :key="card.name"
+      :size="card.size"
+      :class="card.class"
+      :text="card.title"
+      :image="card.image"
+      @click="$route.push({ name: `${card.name}` })"
     >
     </card-template>
   </q-page>
@@ -29,9 +16,48 @@
 
 <script>
 import { defineComponent, defineAsyncComponent } from "vue";
-
+import { useQuasar } from "quasar";
 export default defineComponent({
   name: "IndexPage",
+  setup() {
+    const mobile = useQuasar();
+    const cards = [
+      {
+        name: "all-data",
+        size: mobile.platform.is.mobile ? "medium" : "large",
+        class: "home__card-main",
+        title: "Todos os Dados",
+        image: "src/assets/images/all-data.jpg",
+      },
+      {
+        name: "per-gender",
+        size: mobile.platform.is.mobile ? "small" : "medium",
+        class: "home__card-perGender",
+        title: "Por Gênero",
+        image: "src/assets/images/other.jpg",
+      },
+      {
+        name: "per-education",
+        size: "small",
+        class: "home__card",
+        title: "Por Grau de Instrução",
+        image: "src/assets/images/other.jpg",
+      },
+      {
+        name: "per-race",
+        size: "small",
+        class: "home__card",
+        title: "Por Cor/Raça",
+        image: "src/assets/images/other.jpg",
+      },
+    ];
+    return {
+      test: () => {
+        console.log("teste");
+      },
+      cards,
+    };
+  },
   components: {
     CardTemplate: defineAsyncComponent(() =>
       import("components/CardTemplate.vue")
@@ -48,25 +74,17 @@ export default defineComponent({
   @media (min-width: $breakpoint-sm) {
     display: grid;
     grid-template-columns: repeat(2, auto);
-    grid-template-rows: repeat(3, auto);
-    padding: 2rem 8rem 2rem 8rem;
+    grid-template-rows: repeat(4, auto);
+    padding: 3rem 10rem 3rem 10rem;
     gap: 2.5rem;
 
-    &__card-large {
+    &__card-main {
       grid-column: 1 / 2;
-      grid-row: 1 / 4;
+      grid-row: 1 / 5;
     }
-    &__card-second {
+    &__card-perGender {
       grid-column: 2 / 3;
-      grid-row: 1 / 2;
-    }
-    &__card-third {
-      grid-column: 2 / 3;
-      grid-row: 2 / 3;
-    }
-    &__card-fourth {
-      grid-column: 2 / 3;
-      grid-row: 3 / 4;
+      grid-row: 1 / 3;
     }
   }
 }
